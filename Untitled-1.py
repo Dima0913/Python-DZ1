4dc57a2df552b4d9f316884d1663ecb44819c9ab
Dima0913 <dimarovno2009@gmail.com>
2023-12-17 12:09:23 +0200

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,36 @@
+#Task 1
+# number1 = int(input("enter your first number "))
+# print (type(number1))
+# number2 = int(input("enter your second number "))
+# print(type(number2))
+# number3 = int(input("enter number3 "))
+# print (type(number3))
+# print (number1 + number2 + number3)
+# print (number1 * number2 * number3)
+
+
+
+
+
+
+
+
+
+#Task 2
+# zarplata = int(input("enter your zarplta: "))
+# print (type(zarplata))
+# number2 = int(input("enter your number2: "))
+# print (type(number2))
+# number3 = int(input("enter your number3: "))
+# print (type(number3))
+# print (f"zalyshok = {zarplata - (number2 + number3)}")
+
+
+
+
+#Task 3
+# d1 = int(input("enter your first diagonal: "))
+# print (type(d1))
+# d2 = int(input("enter your second diagondal: "))
+# print (type(d2))
+# print (f"S = {d1*d2/2}")
